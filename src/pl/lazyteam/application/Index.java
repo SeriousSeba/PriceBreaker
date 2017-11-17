8b2dff99b8710870c647980b6348fde876094dfa
SeBanas <banaseb@gmail.com>
2017-11-17 15:42:56 +0100

    Initial commit

---
+++ src/pl/lazyteam/application/Index.java
@@ -0,0 +1,15 @@
+package pl.lazyteam.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class Index {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+        model.addAttribute("message", "Dziala!");
+        return "index";
+    }
+}
